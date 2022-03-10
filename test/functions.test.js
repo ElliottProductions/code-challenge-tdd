bca727e4d480008b70d465eef1c77063bbba6954
Elliott Cheifetz <elliottsmyname@gmail.com>
2022-03-10 14:31:33 -0800

    created 3 of each test

--- test/functions.test.js
+++ test/functions.test.js
@@ -22,6 +22,14 @@ test('addExclamationPoints2', (expect) => {
     expect.equal(actual, expected);
 });
 
+test('this adds exclamation points', (expect) => {
+    const expected = 'Exclaim!!!';
+
+    const actual = addExclamationPoints('Exclaim');
+
+    expect.equal(actual, expected);
+});
+
 
 
 test('multiplyBySeven', (expect) => {
@@ -40,10 +48,34 @@ test('multiplyBySeven2', (expect) => {
     expect.equal(actual, expected);
 });
 
+test('this multiplies by 7', (expect) => {
+    const expected = 63;
+
+    const actual = multiplyBySeven(9);
+
+    expect.equal(actual, expected);
+});
+
 test('this test multiplyBy12ThenHalve', (expect) => {
     const expected = 24;
 
     const actual = multiplyBy12ThenHalve(4);
+
+    expect.equal(actual, expected);
+});
+
+test('this test multiplyBy12ThenHalve2', (expect) => {
+    const expected = 42;
+
+    const actual = multiplyBy12ThenHalve(7);
+
+    expect.equal(actual, expected);
+});
+
+test('this mlutiplies by 12 then divides in half', (expect) => {
+    const expected = 66;
+
+    const actual = multiplyBy12ThenHalve(11);
 
     expect.equal(actual, expected);
 });
@@ -56,10 +88,42 @@ test('this test returnAsAnArray', (expect) => {
     expect.deepEqual(actual, expected);
 });
 
+test('this test returnAsAnArray2', (expect) => {
+    const expected = [4, 2, 0];
+
+    const actual = returnAsAnArray(4, 2, 0);
+
+    expect.deepEqual(actual, expected);
+});
+
+test('This returns three inputs as an array', (expect) => {
+    const expected = ['dream', 'team', 'time'];
+
+    const actual = returnAsAnArray('dream', 'team', 'time');
+
+    expect.deepEqual(actual, expected);
+});
+
 test('this test returnAsAString', (expect) => {
     const expected = '845';
 
     const actual = returnAsAString(8, 4, 5);
+
+    expect.equal(actual, expected);
+});
+
+test('this test returnAsAString2', (expect) => {
+    const expected = '420';
+
+    const actual = returnAsAString(4, 2, 0);
+
+    expect.equal(actual, expected);
+});
+
+test('this returns three numbers as a string', (expect) => {
+    const expected = '777';
+
+    const actual = returnAsAString(7, 7, 7);
 
     expect.equal(actual, expected);
 });
@@ -72,10 +136,42 @@ test('this test makeLuckyGreeting', (expect) => {
     expect.equal(actual, expected);
 });
 
+test('this test makeLuckyGreeting2', (expect) => {
+    const expected = 'Hello! Your lucky number for the day is 13.';
+
+    const actual = makeLuckyGreeting(12, 1);
+
+    expect.equal(actual, expected);
+});
+
+test('this test this tells you your lucky number', (expect) => {
+    const expected = 'Hello! Your lucky number for the day is 3.';
+
+    const actual = makeLuckyGreeting(1, 2);
+
+    expect.equal(actual, expected);
+});
+
 test('this test getSecondItem', (expect) => {
     const expected = 'apple';
 
     const actual = getSecondItem(['kiwi', 'apple', 'orange', 'plum']);
+
+    expect.equal(actual, expected);
+});
+
+test('this test getSecondItem2', (expect) => {
+    const expected = 'frisby';
+
+    const actual = getSecondItem(['basketball', 'frisby', 'traffic cone', 'boof']);
+
+    expect.equal(actual, expected);
+});
+
+test('this returns second item in an array', (expect) => {
+    const expected = 'blue';
+
+    const actual = getSecondItem(['orange', 'blue', 'white', 'magenta']);
 
     expect.equal(actual, expected);
 });
@@ -88,7 +184,23 @@ test('this test getLastItem', (expect) => {
     expect.equal(actual, expected);
 });
 
-test('this test getRandomNumber', (expect) => {
+test('this test getLastItem2', (expect) => {
+    const expected = 'boof';
+
+    const actual = getLastItem(['basketball', 'frisby', 'traffic cone', 'boof']);
+
+    expect.equal(actual, expected);
+});
+
+test('this returns the last item in an array', (expect) => {
+    const expected = 'magenta';
+
+    const actual = getLastItem(['orange', 'blue', 'white', 'magenta']);
+
+    expect.equal(actual, expected);
+});
+
+test('this delivers a random number', (expect) => {
 
     const answer = getRandomNumber();
 
